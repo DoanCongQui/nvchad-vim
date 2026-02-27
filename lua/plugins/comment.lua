81228255a49abc3b6fcd99ff730d942b234c4fac
DoanCongQui <congt4644@gmail.com>
2026-02-27 09:50:08 +0700

    remove lazy vim

--- lua/plugins/comment.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "preservim/nerdcommenter",
-  keys = {
-    { "++", "<plug>NERDCommenterToggle", mode = "n", desc = "Toggle Comment" },
-    { "++", "<plug>NERDCommenterToggle", mode = "v", desc = "Toggle Comment (Visual)" },
-  },
-}
